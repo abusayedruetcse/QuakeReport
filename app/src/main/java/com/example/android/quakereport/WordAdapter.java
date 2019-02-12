2b532379d489c7c835ce8cc285d83d735b49066d
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-02-12 14:44:41 +0600

    Adding drawable circle background for magnitude

--- app/src/main/java/com/example/android/quakereport/WordAdapter.java
+++ app/src/main/java/com/example/android/quakereport/WordAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.quakereport;
 
 import android.content.Context;
 import android.location.Location;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,8 @@ import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+
+import android.graphics.drawable.GradientDrawable;
 
 public class WordAdapter extends ArrayAdapter<Word> {
     private static final String stringSeparator="of";
@@ -35,6 +38,17 @@ public class WordAdapter extends ArrayAdapter<Word> {
         String outputMagnitude = formatter.format(magnitude);
         TextView textMagnitude=(TextView)listItemView.findViewById(R.id.magnitude);
         textMagnitude.setText(outputMagnitude);
+
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) textMagnitude.getBackground();
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = this.getMagnitudeColor(magnitude);  //calling method self defined.
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
+
         //splitting location into offset and primary
         String offsetLocation;
         String primaryLocation;
@@ -71,11 +85,44 @@ public class WordAdapter extends ArrayAdapter<Word> {
         TextView textTime=(TextView)listItemView.findViewById(R.id.time);
         textTime.setText(timeToDisplay);
 
-
-
-
-
-
         return listItemView;
+    }
+    private int getMagnitudeColor(double magnitude) {
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
     }
 }
